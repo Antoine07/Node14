4605ca0f8ad384ee095f793faa102ef5b67fadad
Antoine07 <antoine.lucsko@gmail.com>
2022-05-03 10:37:56 +0200

    read

--- semaine_01_fondamentaux/J1_introduction/02_Introduction_J1/Exercices/02_read_students/index.js
+++ semaine_01_fondamentaux/J1_introduction/02_Introduction_J1/Exercices/02_read_students/index.js
@@ -11,34 +11,62 @@
 
 -- exercices supplémentaires
 
-6. Ajoutez dans le fichier students.text les étudiants suivants :
+6. Ajoutez dans le fichier students.txt les étudiants suivants :
 
-7. Récupérez tous les étudiants habitant à Paris et placez les dans un nouveau fichier.
+- 18 Sonia Paris
+
+- 17 Clarisse Marseille
+
+7. Lire le fichier lui-même et mettez chaque nom en majuscule.
 
 */
 
-import { appendFile, writeFile, readFileSync, readFile } from "fs";
+import { appendFile, writeFile, readFileSync, readFile, appendFileSync } from "fs";
 
-readFile("./Data/students.txt", "utf-8", (err, data) => {
+const urlStudent = "./Data/students.txt";
+
+readFile(urlStudent, "utf-8", (err, data) => {
   if (err) {
     console.error(err);
     return;
   }
 
- //  console.log(data);
+  //  console.log(data);
 });
+
 // synchrone
-const students = readFileSync("./Data/students.txt", "utf-8").split("\n").map(s => s.split(' ')).filter(s => s[0] != 'Notes' );
+let students = readFileSync(urlStudent, "utf-8")
+  .split("\n")
+  .map((s) => s.split(" "))
+  .filter((s) => s[0] != "Notes");
+
+
 console.log(students);
 
 // Plus de 17
-const more17 = students.filter( s => s[0] > 17 );
+const more17 = students.filter((s) => s[0] > 17);
 console.log(more17);
-const bestStudent = students.reduce((acc, curr) =>  (curr[0] > acc[0]) ? curr :  acc ) 
+const bestStudent = students.reduce((acc, curr) =>
+  curr[0] > acc[0] ? curr : acc
+);
 console.log(bestStudent);
 
 // Ordonnez les données dans le tableau
 
-students.sort( (s1, s2) => s1[0] - s2[0] );
+students.sort((s1, s2) => s1[0] - s2[0]);
 
 console.log(students);
+
+
+// 6 & 7
+
+appendFileSync(urlStudent, "\n18 Sonia Paris\n17 Clarisse Marseille", 'utf-8');
+
+students = readFileSync(urlStudent, "utf-8")
+  .split("\n")
+  .map((s) => s.split(" "))
+  .filter((s) => s[0] != "Notes");
+
+  students = students.map( ([note, name, address]) => ([note, name.toUpperCase(), address]) )
+
+  console.log(students);
