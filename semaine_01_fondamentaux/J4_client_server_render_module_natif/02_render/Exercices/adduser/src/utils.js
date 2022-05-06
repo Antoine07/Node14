6d0b1b22b8e76ed8b0d5de5aef86e35484966340
Antoine07 <antoine.lucsko@gmail.com>
2022-05-06 18:17:47 +0200

    refactoring controller and routes

--- semaine_01_fondamentaux/J4_client_server_render_module_natif/02_render/Exercices/adduser/src/utils.js
+++ semaine_01_fondamentaux/J4_client_server_render_module_natif/02_render/Exercices/adduser/src/utils.js
@@ -1,4 +1,8 @@
-const PRECISION = 100; 
+import { dirname } from "path";
+import url, { fileURLToPath } from "url";
+
+export const __dirname = dirname(fileURLToPath(import.meta.url));
+export const PRECISION = 100;
 
 export function s(name) {
   return name.toLocaleLowerCase().trim();
@@ -10,26 +14,27 @@ export function error404({ ejs, title }) {
   return ejs.render(page404, { title: "Page Not Found 404" });
 }
 
-
-export function avg(notes){
-
-  if(notes.length > 0 )
-    return Math.floor( ( notes.reduce((acc, curr) => acc + curr ) /notes.length) * PRECISION ) / PRECISION;
+export function avg(notes) {
+  if (notes.length > 0)
+    return (
+      Math.floor(
+        (notes.reduce((acc, curr) => acc + curr) / notes.length) * PRECISION
+      ) / PRECISION
+    );
 
   return null;
 }
 
 // On ne l'utilise plus car on a un module, formidable, qui parse maintenant les données POST
-export function parser(body){
+export function parser(body) {
   // "name=Harry+Potter+XII&age=12"
   // [ [ 'name', 'Harry Potter XII' ], [ 'age', '12' ] ]
   //[ { name : 'Harry Potter XII'  , age : 12 }]
 
-  let struct = { };
+  let struct = {};
 
-  for(const s of body.split('&') ){
-
-    if(s.split('=')[0] === 'avatar') continue ;
+  for (const s of body.split("&")) {
+    if (s.split("=")[0] === "avatar") continue;
 
     // struct = { ...struct, [ s.split('=')[0] ] : s.split('=')[1].replaceAll('+', ' ').trim()  }
   }
@@ -37,9 +42,18 @@ export function parser(body){
   return struct;
 }
 
-export function showMessage({message, old, type}) {
-
-  return ({
-    message, type, old
-  })
+export function showMessage({ content, old, type }) {
+  return {
+    content,
+    type,
+    old,
+  };
 }
+
+export const message = [
+  {
+    content : '',
+    type : 'Ok',
+    old : null,
+  },
+];
